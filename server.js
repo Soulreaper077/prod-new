214e16bce470fd9e6d56fe8492d8250361e6391a
Jesse <jesseponce233@gmail.com>
2022-12-01 17:47:31 -0600

    basic html and server code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(
+process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/soul-social',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
