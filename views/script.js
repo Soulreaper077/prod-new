b9f6f2b20da65b17bc9b993fd51cf21908a6c76f
Jesse <jesseponce233@gmail.com>
2022-12-19 21:22:25 -0600

    working with the scripts for weather

--- views/script.js
+++ views/script.js
@@ -1,1 +1,15 @@
 // scripts for the weather api front end below 
+// constants below 
+var count  = 0;
+var searchEl = document.getElementById('searchBtn');
+var clearEl = document.getElementById('clearBtn');
+
+searchEl.addEventListener('click', function () {
+    console.log('clicked');
+    count++
+});
+
+clearEl.addEventListener('click', function(){
+    console.log('clicked');
+    count++
+}); 
